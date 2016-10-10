ed0eeb3852d64b9c8d379744841d61914e6f19d7
Jonathan Hudak <hudak.jonathan@gmail.com>
2016-10-10 14:00:43 -0700

    Loader Component

---
+++ src/components/Loader.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../stylus/modules/loader';
+export default class extends Component {
+  render() {
+    if (!this.props.show) {
+      return null;
+    }
+    return(
+      <div className="sk-folding-cube">
+        <div className="sk-cube1 sk-cube"></div>
+        <div className="sk-cube2 sk-cube"></div>
+        <div className="sk-cube4 sk-cube"></div>
+        <div className="sk-cube3 sk-cube"></div>
+      </div>
+    );
+  }
+}
