67a4945df1f702bf2874df051337ed97e0321289
Ivan Cruz <ivan@shiftsave.com>
2016-12-23 12:22:17 -0800

    fix border styles

--- src/routes/project/components/Script.js
+++ src/routes/project/components/Script.js
@@ -8,6 +8,7 @@ const Script = () => {
         <p>In today’s connected world, work does not have to be confined to the office. Being productive is having the ability to share and collaborate on information with virtually anywhere in the world. So how can you take your great local team and turn it into a world-class team?</p>
         <p>By foregoing the limitations of the office and allowing team members to contribute regardless of their location. Microsoft Cloud allows employees to work from virtually anywhere, the office, home or even on the move.</p>
         <p>Cloud computing is here and Microsoft has both the knowledge and capabilities to provide you and your company with the necesary tools to thrive in today’s world.</p>
+        <div className='border' />
       </div>
     </div>
   )
