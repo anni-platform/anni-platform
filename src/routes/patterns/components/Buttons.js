6f9cfc140b220086d62680f1723fdcf1d0fa9959
shiftsave <ivan@shiftsave.com>
2017-04-04 22:40:21 +1200

    stub out pattern library page

---
+++ src/routes/patterns/components/Buttons.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Card from 'components/Card';
+import Icon from 'components/Icon';
+
+const buttons = () => (
+  <div>
+    <h4 className='section'>buttons</h4>
+    <Card>
+      <h4 className='legend'>Regular buttons</h4>
+      <div>
+        <button className='primary'>Primary</button>
+        <button className='default'>Default</button>
+        <button className='info'>Info</button>
+        <button className='link'>Link</button>
+      </div>
+
+      <h4>Icon buttons</h4>
+      <div>
+        <button className='primary'>
+          <Icon name='plus' />Primary
+        </button>
+        <button className='default'>
+          <Icon name='plus' />Default
+        </button>
+        <button className='info'>
+          <Icon name='plus' />Info
+        </button>
+        <button className='link'>
+          <Icon name='arrow-back' />Link
+        </button>
+      </div>
+    </Card>
+  </div>
+);
+
+export default buttons;
---
+++ src/routes/patterns/components/Panels.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Card from 'components/Card';
+
+const Panels = () => (
+  <Card>
+  </Card>
+);
+
+export default Panels;
