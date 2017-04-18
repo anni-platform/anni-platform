04c81b943ea517b780d0e5b1e645952683ccad3a
shiftsave <ivan@shiftsave.com>
2017-04-18 14:37:16 +1200

    style changes pattern library

--- src/routes/patterns/components/Buttons.js
+++ src/routes/patterns/components/Buttons.js
@@ -2,34 +2,31 @@ import React from 'react';
 import { Card, Icon } from 'components/UI';
 
 const buttons = () => (
-  <div>
-    <h4 className='section'>buttons</h4>
-    <Card>
-      <h4 className='legend'>Regular buttons</h4>
-      <div>
-        <button className='primary'>Primary</button>
-        <button className='default'>Default</button>
-        <button className='info'>Info</button>
-        <button className='link'>Link</button>
-      </div>
+  <Card>
+    <h4 className='legend'>Regular buttons</h4>
+    <div>
+      <button className='primary'>Primary</button>
+      <button className='default'>Default</button>
+      <button className='info'>Info</button>
+      <button className='link'>Link</button>
+    </div>
 
-      <h4>Icon buttons</h4>
-      <div>
-        <button className='primary'>
-          <Icon name='plus' />Primary
-        </button>
-        <button className='default'>
-          <Icon name='plus' />Default
-        </button>
-        <button className='info'>
-          <Icon name='plus' />Info
-        </button>
-        <button className='link'>
-          <Icon name='arrow-back' />Link
-        </button>
-      </div>
-    </Card>
-  </div>
+    <h4>Icon buttons</h4>
+    <div>
+      <button className='primary'>
+        <Icon name='plus' />Primary
+      </button>
+      <button className='default'>
+        <Icon name='plus' />Default
+      </button>
+      <button className='info'>
+        <Icon name='plus' />Info
+      </button>
+      <button className='link'>
+        <Icon name='arrow-back' />Link
+      </button>
+    </div>
+  </Card>
 );
 
 export default buttons;
