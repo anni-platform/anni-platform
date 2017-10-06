bb277de37a386cd2ecef39f5735c1f36cd0ec863
shiftsave <ivan@shiftsave.com>
2017-10-06 10:19:53 -0500

    styles

--- src/routes/patterns/components/Typography.js
+++ src/routes/patterns/components/Typography.js
@@ -6,35 +6,33 @@ const Typography = () => (
     <Subheading capitalize color micro mb={8}>Heading</Subheading>
     <Heading>Echo Easy Foxtrot Fox</Heading>
     <CodeBlock>
-{`import { Heading } from "styled";
+{
+`import { Heading } from "styled";
 <Heading>Text here</Heading>`
 }
     </CodeBlock>
-
     <Subheading capitalize color micro mb={8}>Subheading</Subheading>
     <Subheading>Kilo King Lima Love</Subheading>
-    <CodeBlock mb={24}>{`<Subheading>Text here</Subheading>`}</CodeBlock>
-
-    <Subheading capitalize color micro mb={8}>Tiny</Subheading>
-    <Subheading tiny>India Item Juliet Jig</Subheading>
     <CodeBlock mb={24}>
-{`import { Heading } from "styled";
-<Subheading tiny>Text here</Subheading>`
+{
+`import { Subheading } from "styled"
+<Subheading>Text here</Subheading>
+
+/* Props
+   capitalize  Transform to uppercase
+   center      Center horizontally
+   color       Display with accent color
+   tiny        Smaller font-size
+   micro       Smallest font-size
+ */
+`
 }
     </CodeBlock>
-
-    <Subheading capitalize color micro mb={8}>Micro</Subheading>
-    <Subheading capitalize micro>Sierra Sugar Tanto Tare</Subheading>
-    <CodeBlock mb={24}>
-{`import { Heading } from "styled";
-<Subheading micro>Text here</Subheading>`
-}
-    </CodeBlock>
-
     <Subheading capitalize color micro mb={8}>Paragraph</Subheading>
     <Paragraph>Apples Butter Charlie Duff Edward Freddy George Harry Ink Johnnie King London Monkey Nuts Orange Pudding Queenie Robert Sugar Tommy Uncle Vinegar Willie Xerxes Yellow Zebra Nuts Orange Pudding.</Paragraph>
     <CodeBlock mb={24}>
-{`import { Heading } from "styled";
+{
+`import { Paragraph } from "styled";
 <Paragraph>Text here</Paragraph>`
 }
     </CodeBlock>
